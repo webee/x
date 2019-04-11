851dca3f428b205c68949f08a14696dbafb04043
webee.yw <webee.yw@gmail.com>
2019-04-11 16:35:26 +0800

    add xswagger app

--- go.mod
+++ go.mod
@@ -6,5 +6,6 @@ require (
 	github.com/BurntSushi/toml v0.3.1 // indirect
 	github.com/jinzhu/configor v1.0.0
 	github.com/joho/godotenv v1.3.0
-	gopkg.in/yaml.v2 v2.2.2 // indirect
+	github.com/labstack/echo/v4 v4.0.0
+	github.com/swaggo/echo-swagger v0.0.0-20190329130007-1219b460a043
 )
---
+++ xswagger/app/app.go
@@ -0,0 +1,68 @@
+package app
+
+import (
+	"encoding/json"
+	"net/http"
+	"os"
+
+	"github.com/labstack/echo/v4"
+	"github.com/labstack/echo/v4/middleware"
+
+	"github.com/swaggo/echo-swagger"
+)
+
+// App is app object
+type App struct {
+	config *Config
+	e      *echo.Echo
+}
+
+// Run start server
+func (app *App) Run(address string) {
+	app.e.Logger.Fatal(app.e.Start(address))
+}
+
+// Destroy destroy this app.
+func (app *App) Destroy() {
+}
+
+// CreateApp create a app object
+func CreateApp(debug bool, config *Config) *App {
+	app := &App{config, echo.New()}
+
+	app.e.Debug = debug
+
+	app.e.Use(middleware.Logger())
+	app.e.Use(middleware.Recover())
+	app.e.Use(middleware.CORS())
+
+	// routers
+	app.e.GET("/", redirectToSwaggerIndex)
+
+	app.e.GET(config.DocPath, func(c echo.Context) error {
+		f, err := os.Open(config.DocFile)
+		if err != nil {
+			return err
+		}
+		defer f.Close()
+
+		dec := json.NewDecoder(f)
+		data := make(map[string]interface{}, 0)
+		if err := dec.Decode(&data); err != nil {
+			return err
+		}
+		data["host"] = config.Host
+		return c.JSON(http.StatusOK, data)
+	})
+	if config.Dev {
+		app.e.GET(config.SwaggerPath+"*", echoSwagger.EchoWrapHandler(echoSwagger.URL(config.DocPath)))
+	} else {
+		app.e.GET(config.SwaggerPath+"*", echoSwagger.WrapHandler)
+	}
+
+	return app
+}
+
+func redirectToSwaggerIndex(c echo.Context) error {
+	return c.Redirect(http.StatusMovedPermanently, "/swagger/index.html")
+}
