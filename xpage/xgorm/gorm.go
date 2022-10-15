b27db20b1a5768ea9e47d27c8d9f855c48b912a9
webee.yw <webee.yw@gmail.com>
2022-10-15 18:11:22 +0800

    add pagination option for xgorm

--- xpage/xgorm/gorm.go
+++ xpage/xgorm/gorm.go
@@ -2,28 +2,40 @@ package xgorm
 
 import (
 	"fmt"
+	"reflect"
 
 	"github.com/webee/x/xpage"
 	"gorm.io/gorm"
 )
 
 type gormPaginator struct {
-	tx    *gorm.DB
-	query *gorm.DB
-	items interface{}
+	tx     *gorm.DB
+	query  *gorm.DB
+	items  interface{}
+	option *PaginationOption
+}
+
+type PaginationOption struct {
+	UseSecond bool
 }
 
 // NewPaginator 创建分页器
-func NewPaginator(tx, query *gorm.DB, items interface{}) xpage.Paginator {
+func NewPaginator(tx, query *gorm.DB, items interface{}, option *PaginationOption) xpage.Paginator {
 	return &gormPaginator{
-		tx:    tx,
-		query: query,
-		items: items,
+		tx:     tx,
+		query:  query,
+		items:  items,
+		option: option,
 	}
 }
 
 // Paginate 实现Paginator接口
 func (p *gormPaginator) Paginate(page, perPage int) (*xpage.Pagination, error) {
+	if p.option != nil {
+		if p.option.UseSecond {
+			return p.Paginate2(page, perPage)
+		}
+	}
 
 	if err := p.query.Clauses(Hints{Clauses: []string{"SELECT"}, Content: "SQL_CALC_FOUND_ROWS"}).Offset(xpage.CalcOffset(page, perPage)).Limit(perPage).Find(p.items).Error; err != nil {
 		return nil, fmt.Errorf("pagination error: %v", err)
@@ -32,6 +44,21 @@ func (p *gormPaginator) Paginate(page, perPage int) (*xpage.Pagination, error) {
 	var total int64
 	if err := p.tx.Raw("SELECT FOUND_ROWS()").Scan(&total).Error; err != nil {
 		return nil, fmt.Errorf("pagination count error: %v", err)
+	}
+
+	return xpage.NewPagination(page, perPage, total, p.items), nil
+}
+
+// Paginate2 实现Paginator接口
+func (p *gormPaginator) Paginate2(page, perPage int) (*xpage.Pagination, error) {
+	model := reflect.New(reflect.Indirect(reflect.ValueOf(p.items)).Type().Elem()).Interface()
+	var total int64
+	if err := p.query.Model(model).Count(&total).Error; err != nil {
+		return nil, fmt.Errorf("pagination count error: %v", err)
+	}
+
+	if err := p.query.Offset(xpage.CalcOffset(page, perPage)).Limit(perPage).Find(p.items).Error; err != nil {
+		return nil, fmt.Errorf("pagination error: %v", err)
 	}
 
 	return xpage.NewPagination(page, perPage, total, p.items), nil
